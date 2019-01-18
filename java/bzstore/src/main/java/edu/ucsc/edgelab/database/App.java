92c2d0aefb0389e73df06ab9cca0938831a2e4c6
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-01-18 10:33:27 -0800

    Adding java project for bzstore

---
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/database/App.java
@@ -0,0 +1,13 @@
+package edu.ucsc.edgelab.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
