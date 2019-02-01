af6baa60e07b5643ef39c50949ce40293d3f3205
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-02-01 11:34:10 -0800

    Corrected indentation.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/database/BZStoreServer.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/database/BZStoreServer.java
@@ -2,13 +2,10 @@ package edu.ucsc.edgelab.database;
 
 /**
  * Hello world!
- *
  */
-public class BZStoreServer
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class BZStoreServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
 
     }
 }
