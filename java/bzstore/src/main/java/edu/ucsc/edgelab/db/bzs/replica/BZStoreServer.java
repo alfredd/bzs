6c36f315974862f51ee6d9b92405020b27162568
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-02-06 14:32:35 -0800

    Trying lambdas.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/BZStoreServer.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/BZStoreServer.java
@@ -4,15 +4,20 @@ import edu.ucsc.edgelab.db.bzs.configuration.BZStoreProperties;
 import edu.ucsc.edgelab.db.bzs.exceptions.UnknownConfiguration;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * Hello world!
  */
 public class BZStoreServer {
+
+    private static final Logger logger = Logger.getLogger(BZStoreServer.class.getName());
+
     public static void main(String[] args) throws IOException {
         BZStoreProperties properties = new BZStoreProperties();
         try {
-            String serverPort = properties.getProperty(BZStoreProperties.Configuration.server_port);
+            Integer serverPort = Integer.decode(properties.getProperty(BZStoreProperties.Configuration.server_port));
+            logger.info(String.format("Server port configured at %d", serverPort));
         } catch (UnknownConfiguration unknownConfiguration) {
             unknownConfiguration.printStackTrace();
         }
