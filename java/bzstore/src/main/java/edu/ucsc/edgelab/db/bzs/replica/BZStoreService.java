aa4b84e79d2b74300d5fec01bfdc6fb441e1b074
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-02-06 15:27:57 -0800

    Adding implementation.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/BZStoreService.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/BZStoreService.java
@@ -1,7 +1,23 @@
 package edu.ucsc.edgelab.db.bzs.replica;
 
 import edu.ucsc.edgelab.db.bzs.BZStoreGrpc;
+import edu.ucsc.edgelab.db.bzs.Commit;
+import io.grpc.stub.StreamObserver;
 
 class BZStoreService extends BZStoreGrpc.BZStoreImplBase {
 
+    @Override
+    public void commit(Commit.Transaction request, StreamObserver<Commit.TransactionResponse> responseObserver) {
+        super.commit(request, responseObserver);
+    }
+
+    @Override
+    public void rOCommit(Commit.ROTransaction request, StreamObserver<Commit.ROTransactionResponse> responseObserver) {
+        super.rOCommit(request, responseObserver);
+    }
+
+    @Override
+    public void readOperation(Commit.Read request, StreamObserver<Commit.ReadResponse> responseObserver) {
+        super.readOperation(request, responseObserver);
+    }
 }
