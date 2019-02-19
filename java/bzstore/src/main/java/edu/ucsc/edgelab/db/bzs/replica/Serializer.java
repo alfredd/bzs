6128592ccbcacd5ce9513af62b66d68e8a19c29d
aasimkhan30 <aasimkhan30@gmail.com>
2019-02-19 13:36:47 -0800

    Corrected serializer code. Used getters and setters instead of direct variable access.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/Serializer.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/Serializer.java
@@ -11,7 +11,7 @@ public class Serializer {
 
     private List<Bzs.Transaction> epochList = new LinkedList<>();
     // Keeping track of current object changes to the epochList.
-    private HashMap<String, Integer> readMap = new HashMap<>();
+    private HashMap<String, Long> readMap = new HashMap<>();
 
     public void resetEpoch() {
         epochList.clear();
@@ -19,25 +19,26 @@ public class Serializer {
     }
 
     public boolean readConflicts(Bzs.ReadHistory c, BpTree datastore){
-        if(!readMap.containsKey(c.key)) {
-            readMap.add(datastore.get(key));
+        //Needs to be changes where the version is fetched from the datastore and not the first key.
+        if(!readMap.containsKey(c.getKey())){
+            readMap.put(c.getKey(), c.getVersion());
         }
-        // Handling case 2 and 3 from the table in the doc
-        if(readMap.get(c.key) > c.version){
+        // Handling case 2 and 3 from the table in the google doc
+        if(readMap.get(c.getKey()) > c.getVersion()){
             return false;
         }
         return true;
     }
 
     public boolean serialize(Bzs.Transaction t, BpTree datastore) {
-        for(Bzs.ReadHistory object : t.readHistory){
+        for(Bzs.ReadHistory object : t.getReadHistoryList()){
             if(readConflicts(object,datastore))
                 return false;
         }
-        // Handling case 2 from the table in the doc
-        for(Bzs.writeOperations object : t.writeHistory){
-            int version = readMap.get(object.key);
-            readMap.put(object.key, version + 1);
+        // Handling case 2 from the table in the google doc
+        for(Bzs.Write object : t.getWriteOperationsList()){
+            long version = readMap.get(object.getKey());
+            readMap.put(object.getKey(), version + 1);
         }
         epochList.add(t);
         return true;
