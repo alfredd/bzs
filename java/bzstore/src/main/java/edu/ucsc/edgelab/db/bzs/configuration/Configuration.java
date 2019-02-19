c7ea2799b53ddd1564aaed30e505c9795ceb3574
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-02-18 21:17:47 -0800

    Minor

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/configuration/Configuration.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/configuration/Configuration.java
@@ -3,12 +3,10 @@ package edu.ucsc.edgelab.db.bzs.configuration;
 import edu.ucsc.edgelab.db.bzs.exceptions.UnknownConfiguration;
 
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Configuration {
 
-    ServerInfo getLeaderInfo() throws IOException, UnknownConfiguration {
+    public ServerInfo getLeaderInfo() throws IOException, UnknownConfiguration {
         ServerInfo info = new ServerInfo();
         BZStoreProperties properties = new BZStoreProperties();
 
--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/BZStoreService.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/BZStoreService.java
@@ -2,23 +2,53 @@ package edu.ucsc.edgelab.db.bzs.replica;
 
 import edu.ucsc.edgelab.db.bzs.BZStoreGrpc;
 import edu.ucsc.edgelab.db.bzs.Bzs;
+import edu.ucsc.edgelab.db.bzs.configuration.Configuration;
+import edu.ucsc.edgelab.db.bzs.configuration.ServerInfo;
+import edu.ucsc.edgelab.db.bzs.exceptions.UnknownConfiguration;
 import io.grpc.stub.StreamObserver;
 
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class BZStoreService extends BZStoreGrpc.BZStoreImplBase {
 
     private static final Logger log = Logger.getLogger(BZStoreService.class.getName());
     private final String id;
+    private Configuration configuration;
 
     public BZStoreService(String id) {
         log.info("BZStore service started. Replica ID: " + id);
-        this.id=id;
+        this.id = id;
+        configuration = new Configuration();
     }
 
     @Override
     public void commit(Bzs.Transaction request, StreamObserver<Bzs.TransactionResponse> responseObserver) {
-        super.commit(request, responseObserver);
+
+        Bzs.TransactionResponse response;
+        try {
+            ServerInfo leader = configuration.getLeaderInfo();
+            if (leader.id.equals(id)) {
+                // If this instance is the leader process the transaction.
+
+            } else {
+                // If this instance is not the leader forward transaction to the leader.
+
+            }
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Could not connect to leader. Aborting transaction.", e);
+            response = Bzs.TransactionResponse.newBuilder().setStatus(Bzs.TransactionStatus.ABORTED).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (UnknownConfiguration unknownConfiguration) {
+            log.log(Level.SEVERE, "Could not connect to leader due to configuration error. " +
+                    "Aborting transaction.", unknownConfiguration);
+            response = Bzs.TransactionResponse.newBuilder().setStatus(Bzs.TransactionStatus.ABORTED).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+
     }
 
     @Override
