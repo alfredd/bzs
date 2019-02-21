73d528d0260bfc10974c0d7c9cb4a260a8e9f871
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-02-20 20:13:30 -0800

    Tentative implementation of the database commit.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/BpTree.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/BpTree.java
@@ -1,5 +1,6 @@
 package edu.ucsc.edgelab.db.bzs.data;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeMap;
 
@@ -8,4 +9,18 @@ import java.util.TreeMap;
  */
 public class BpTree extends TreeMap<String, List<BZStoreData>> {
 
+
+    public void commit(String key, BZStoreData data) {
+        /*
+            not sure if this is the best way to do this.
+         */
+        synchronized (this) {
+            List<BZStoreData> list = this.get(key);
+            if (list==null) {
+                list = new LinkedList<>();
+                this.put(key,list);
+            }
+            list.add(0,data);
+        }
+    }
 }
