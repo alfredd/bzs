2430b546ea4245c1bd93f2667717e01c98facbe9
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-02-23 13:42:03 -0800

    Removing unnecessary code.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/exceptions/InvalidCommitException.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/exceptions/InvalidCommitException.java
@@ -2,14 +2,8 @@ package edu.ucsc.edgelab.db.bzs.exceptions;
 
 public class InvalidCommitException extends Exception {
 
-    public InvalidCommitException() {
-    }
-
     public InvalidCommitException(String message) {
         super(message);
     }
 
-    public InvalidCommitException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
