2291eda88ff33e90a82eee8c963dee23c32ee3ae
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-03-04 13:18:51 -0800

    Transaction processing pipeline.

---
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/ResponseHandlerRegistry.java
@@ -0,0 +1,38 @@
+package edu.ucsc.edgelab.db.bzs.replica;
+
+import edu.ucsc.edgelab.db.bzs.Bzs;
+import io.grpc.stub.StreamObserver;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ResponseHandlerRegistry {
+    private Map<Integer, Map<Integer, StreamObserver<Bzs.TransactionResponse>>> requestHandlerRegistry;
+    private Map<Integer, Map<Integer, Bzs.Transaction>> requestRegistry;
+
+    public void addToRegistry(int epochNumber, int sequenceNumber, Bzs.Transaction transaction,
+                              StreamObserver<Bzs.TransactionResponse> responseObserver) {
+
+        Map<Integer, StreamObserver<Bzs.TransactionResponse>> epochHistory = requestHandlerRegistry.get(epochNumber);
+        if (epochHistory==null) {
+            epochHistory = new LinkedHashMap<>();
+            requestHandlerRegistry.put(epochNumber,epochHistory);
+        }
+        epochHistory.put(sequenceNumber,responseObserver);
+
+        Map<Integer, Bzs.Transaction> epochTransactionHistory = requestRegistry.get(epochNumber);
+        if (epochTransactionHistory==null) {
+            epochTransactionHistory = new LinkedHashMap<>();
+            requestRegistry.put(epochNumber,epochTransactionHistory);
+        }
+        epochTransactionHistory.put(sequenceNumber,transaction);
+
+    }
+
+
+    public void clearEpochHistory(int epochNumber) {
+        requestRegistry.remove(epochNumber);
+        requestHandlerRegistry.remove(epochNumber);
+    }
+
+}
