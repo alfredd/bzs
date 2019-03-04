9d5f917aeab3c083c3ebe6d08eac61a4a2d9ab7c
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-03-04 13:30:16 -0800

    Transaction processing pipeline.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/TransactionProcessor.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/TransactionProcessor.java
@@ -8,14 +8,23 @@ public class TransactionProcessor {
     private Serializer serializer;
     private int sequenceNumber;
     private int epochNumber;
+    private ResponseHandlerRegistry responseHandlerRegistry;
 
     public TransactionProcessor() {
         serializer = new Serializer();
         sequenceNumber = 0;
         epochNumber = 0;
+        responseHandlerRegistry = new ResponseHandlerRegistry();
     }
 
     void processTransaction(Bzs.Transaction request, StreamObserver<Bzs.TransactionResponse> responseObserver) {
-//        serializer.serialize(request);
+        if (serializer.serialize(request) == false) {
+
+            Bzs.TransactionResponse response = Bzs.TransactionResponse.newBuilder().setStatus(Bzs.TransactionStatus.ABORTED).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+            return;
+        }
+        responseHandlerRegistry.addToRegistry(epochNumber,sequenceNumber+1,request,responseObserver);
     }
 }
