252e1a0b82139008f8f14c825b6fbaa178469c0e
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-03-04 10:34:02 -0800

    Refactoring the code.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/bftcommit/BFTClient.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/bftcommit/BFTClient.java
@@ -11,20 +11,20 @@ public class BFTClient {
     private ServiceProxy serviceProxy;
 
 
-    public BFTClient(int ClientId){
+    public BFTClient(int ClientId) {
         serviceProxy = new ServiceProxy(ClientId);
     }
 
-    public List performCommit(List<Bzs.Transaction> t){
+    public List performCommit(List<Bzs.Transaction> rotransactions) {
         LinkedList<Long> result = new LinkedList<>();
         try (ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-             ObjectOutput objOut = new ObjectOutputStream(byteOut)){
-            List<byte[]> b = new LinkedList<>();
-            for(Bzs.Transaction i : t){
-                b.add(i.toByteArray());
+             ObjectOutput objOut = new ObjectOutputStream(byteOut)) {
+            List<byte[]> transactionInBytes = new LinkedList<>();
+            for (Bzs.Transaction i : rotransactions) {
+                transactionInBytes.add(i.toByteArray());
             }
-            objOut.writeObject(new Integer(0));
-            objOut.writeObject(b);
+            objOut.writeObject(0);
+            objOut.writeObject(transactionInBytes);
             objOut.flush();
             byteOut.flush();
 
@@ -33,32 +33,30 @@ public class BFTClient {
                 reply = serviceProxy.invokeOrdered(byteOut.toByteArray());
                 if (reply.length == 0)
                     throw new IOException();
-            }
-            catch (RuntimeException e){
+            } catch (RuntimeException e) {
                 System.out.println("Commit Failed: " + e.getMessage());
                 return result;
             }
-            try(ByteArrayInputStream byteIn = new ByteArrayInputStream(reply);
-                ObjectInput objIn = new ObjectInputStream(byteIn)){
-                result = (LinkedList<Long>)objIn.readObject();
+            try (ByteArrayInputStream byteIn = new ByteArrayInputStream(reply);
+                 ObjectInput objIn = new ObjectInputStream(byteIn)) {
+                result = (LinkedList<Long>) objIn.readObject();
                 System.out.println(result.toString());
             }
-        }
-        catch (IOException | ClassNotFoundException e){
-            System.out.print("Exception generated while committing transaction"+e.getMessage());
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.print("Exception generated while committing transaction" + e.getMessage());
         }
         return result;
     }
 
-    public boolean performRead(List<Bzs.ROTransaction> t){
-        boolean status = false;
-        try(ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-            ObjectOutput objOut = new ObjectOutputStream(byteOut)){
+    public boolean performRead(List<Bzs.ROTransaction> t) {
+        boolean status;
+        try (ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+             ObjectOutput objOut = new ObjectOutputStream(byteOut)) {
             List<byte[]> b = new LinkedList<>();
-            for(Bzs.ROTransaction i : t){
+            for (Bzs.ROTransaction i : t) {
                 b.add(i.toByteArray());
             }
-            objOut.writeObject(new Integer(1));
+            objOut.writeObject(1);
             objOut.writeObject(b);
             objOut.flush();
             byteOut.flush();
@@ -68,15 +66,13 @@ public class BFTClient {
                 if (reply.length == 0)
                     throw new IOException();
                 status = true;
-            }
-            catch (RuntimeException e){
+            } catch (RuntimeException e) {
                 System.out.println("Commit Failed" + e.getMessage());
                 status = false;
                 return status;
             }
-        }
-        catch (IOException e){
-            System.out.print("Exception generated while committing transaction"+e.getMessage());
+        } catch (IOException e) {
+            System.out.print("Exception generated while committing transaction" + e.getMessage());
             status = false;
         }
         return status;
