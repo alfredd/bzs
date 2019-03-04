74f5a94ef21cea9019a91857bd899b5af6a927f5
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-03-04 13:05:21 -0800

    Transaction processing pipeline.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/TransactionProcessor.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/TransactionProcessor.java
@@ -6,9 +6,13 @@ import io.grpc.stub.StreamObserver;
 public class TransactionProcessor {
 
     private Serializer serializer;
+    private int sequenceNumber;
+    private int epochNumber;
 
     public TransactionProcessor() {
         serializer = new Serializer();
+        sequenceNumber = 0;
+        epochNumber = 0;
     }
 
     void processTransaction(Bzs.Transaction request, StreamObserver<Bzs.TransactionResponse> responseObserver) {
