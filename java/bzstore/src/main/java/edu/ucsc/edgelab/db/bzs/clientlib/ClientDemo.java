04aad1a95fd2124fd8a01a95f2931a39e5b2a5cd
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-03-13 15:01:56 -0700

    Another bug found: Replica's not synchronized.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/clientlib/ClientDemo.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/clientlib/ClientDemo.java
@@ -1,5 +1,6 @@
 package edu.ucsc.edgelab.db.bzs.clientlib;
 
+import edu.ucsc.edgelab.db.bzs.configuration.BZStoreProperties;
 import edu.ucsc.edgelab.db.bzs.configuration.Configuration;
 import edu.ucsc.edgelab.db.bzs.configuration.ServerInfo;
 
@@ -30,7 +31,11 @@ public class ClientDemo {
 //        t2.write("x",vx+45);
 //        t2.write("z","5");
 //        t2.commit();
-        Transaction t3 = new Transaction(leader.host,leader.port);
+
+        BZStoreProperties properties = new BZStoreProperties();
+        Integer port1 = Integer.decode(properties.getProperty("1", BZStoreProperties.Configuration.port));
+        String host1 = properties.getProperty("1", BZStoreProperties.Configuration.host);
+        Transaction t3 = new Transaction(host1,port1);
         t3.read("x");
         t3.read("y");
         t3.read("z");
