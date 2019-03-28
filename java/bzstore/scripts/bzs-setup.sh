342dfab548df237a0ba9a20011145b65ce26532f
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-03-28 10:44:19 -0700

    Created a proper script.

--- java/bzstore/scripts/bzs-setup.sh
+++ java/bzstore/scripts/bzs-setup.sh
@@ -1,14 +1,48 @@
 #!/usr/bin/env bash
 
+
+function setup_workspace() {
+    echo "Setting up workspace."
+    sleep 1
+    cp -rf ../../../library/config .
+    cp -f ../../../library/bin/BFT-SMaRt.jar .
+    cp -rf ../target/bzstore-1.0-SNAPSHOT-jar-with-dependencies.jar .
+}
+
 if [[ "$1" == "setup" ]]
 then
-
-    cp -r ../../../library/config .
-    cp ../../../library/bin/BFT-SMaRt.jar .
-    cp ../target/bzstore-1.0-SNAPSHOT-jar-with-dependencies.jar .
+    setup_workspace
 elif [[ "$1" == "clean" ]]
 then
-    rm *.jar
+    echo "Cleaning up workspace."
+    rm -rf *.jar
     rm config -rf
+elif [[ "$1" == "install" ]]
+then
+    java -version
+    if [[ "$?" != "0" ]]
+    then
+        echo "java is not installed. Install java to continue"
+        exit 1
+    fi
+    mvn -version
+    if [[ "$?" != "0" ]]
+    then
+        echo "Maven is not installed. Install mvn to continue"
+        exit 1
+    fi
+    cd ..
+    echo "Building bzstore"
+    mvn clean install
+        if [[ "$?" != "0" ]]
+    then
+        echo "Maven build failed. Rebuild again after fixing errors."
+        exit 1
+    fi
+    cd scripts
+    setup_workspace
+else
+    echo "Usage: $0 setup|install|clean"
+    exit 1
 fi
 
