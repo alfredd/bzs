921cd01c126b937d54e4abe7b7dd10184d0fb971
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-03-27 20:20:02 -0700

    Adding config details.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/configuration/BZStoreProperties.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/configuration/BZStoreProperties.java
@@ -22,7 +22,7 @@ public class BZStoreProperties {
 
         bzsProperties = new Properties();
         String pathname = System.getProperty("user.dir") + "/src/main/resources/" + CONFIG_PROPERTIES;
-        LOGGER.info("Propeties path: "+pathname);
+        LOGGER.info("Properties path: "+pathname);
         bzsProperties.load(
                 new FileInputStream(
                         new File(
