eab4755df221b6a1fa8ac3acde01fb1de116ce10
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-03-29 22:01:19 -0700

    Using RocksDB as data store. Adding as a TODO.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/BackendDb.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/BackendDb.java
@@ -4,6 +4,7 @@ import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class BackendDb {
@@ -13,30 +14,36 @@ class BackendDb {
     public static final Logger LOGGER = Logger.getLogger(BackendDb.class.getName());
 
     BackendDb(int id) throws RocksDBException {
+        // TODO: Setup column families to store version number.
         RocksDB.loadLibrary();
         String dbPath = "BZS_data_" + id;
         Options options = new Options();
         options.setCreateIfMissing(true);
         db = RocksDB.open(options, dbPath);
-        LOGGER.info("RocksDB database file opened: "+dbPath);
+
+        LOGGER.info("RocksDB database file opened: " + dbPath);
     }
 
     void commit(String key, String value) throws RocksDBException {
 
+        boolean loggable = LOGGER.isLoggable(Level.FINE);
         byte[] keyBytes = key.getBytes();
         String prevValue = get(key);
-        LOGGER.info("Input: "+prevValue);
+        if (loggable)
+            LOGGER.log(Level.FINE, "Previous value: " + prevValue);
         long start = System.nanoTime();
         db.put(keyBytes, value.getBytes());
-        LOGGER.info("Total time to write data to DB: " +(System.nanoTime()-start));
+        if (loggable)
+            LOGGER.log(Level.FINE, "Total time to write data to DB: " + (System.nanoTime() - start));
     }
 
-    String get (String key) throws RocksDBException {
+    String get(String key) throws RocksDBException {
         long start = System.nanoTime();
         byte[] value = db.get(key.getBytes());
-        LOGGER.info("Total time to get data from DB: " +(System.nanoTime()-start));
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.log(Level.FINE, "Total time to get data from DB: " + (System.nanoTime() - start));
 
-        return value == null? "" : new String(value);
+        return value == null ? "" : new String(value);
 
     }
 
@@ -46,8 +53,8 @@ class BackendDb {
 
     public static void main(String[] args) throws RocksDBException {
         BackendDb db = new BackendDb(0);
-        db.commit("X","3");
-        LOGGER.info("X="+db.get("X"));
+        db.commit("X", "3");
+        LOGGER.info("X=" + db.get("X"));
         db.close();
     }
 }
