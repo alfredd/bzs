d20b9607d7fb10b6bab8ae9b3d0026ed6799cae4
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-04-11 22:13:22 -0700

    Distributed commits.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/cluster/ClusterClient.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/cluster/ClusterClient.java
@@ -19,9 +19,26 @@ public class ClusterClient {
         return null;
     }
 
+    /**
+     * This is a call to the server to commit the data.
+     * @param transaction
+     * @param clusterID
+     * @return
+     */
     public Bzs.TransactionResponse commit(Bzs.Transaction transaction, Integer clusterID) {
         if (this.clients.containsKey(clusterID))
             return this.clients.get(clusterID).commit(transaction);
         return null;
     }
+
+    /**
+     * Call is exactly the same as commit but execution at server is different. This is a prepare message.
+     * @param transaction
+     * @param clusterID
+     * @return
+     */
+    public Bzs.TransactionResponse commitPrepare(Bzs.Transaction transaction, Integer clusterID) {
+        return commit(transaction, clusterID);
+    }
+
 }
