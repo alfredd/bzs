72eecf7740c615e0dcc789e694ffbb9aa53bbbf7
aasimkhan30 <aasimkhan30@gmail.com>
2019-04-17 11:37:16 -0700

    Added RSA keyGenerator and other Encryption related APIs

---
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/RSAKeyPairGenerator.java
@@ -0,0 +1,61 @@
+package edu.ucsc.edgelab.db.bzs.replica;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.security.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import org.apache.commons.codec.binary.Base64;
+
+public class RSAKeyPairGenerator {
+    //
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+        public RSAKeyPairGenerator() {
+        }
+
+        public void run(int id, int size) throws Exception {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(size);
+            KeyPair kp = keyGen.generateKeyPair();
+            PublicKey puk = kp.getPublic();
+            PrivateKey prk = kp.getPrivate();
+            this.saveToFile(id, puk, prk);
+        }
+
+        private void saveToFile(int id, PublicKey puk, PrivateKey prk) throws Exception {
+            String path = "config" + System.getProperty("file.separator") + "keys" + System.getProperty("file.separator");
+            BufferedWriter w = new BufferedWriter(new FileWriter(path + "publickey" + id, false));
+            w.write(this.getKeyAsString(puk));
+            w.flush();
+            w.close();
+            w = new BufferedWriter(new FileWriter(path + "privatekey" + id, false));
+            w.write(this.getKeyAsString(prk));
+            w.flush();
+            w.close();
+        }
+
+        private String getKeyAsString(Key key) {
+            byte[] keyBytes = key.getEncoded();
+            return Base64.encodeBase64String(keyBytes);
+        }
+
+        public static void main(String[] args) {
+            try {
+                (new bftsmart.tom.util.RSAKeyPairGenerator()).run(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            } catch (Exception var2) {
+                System.out.println(var2.getMessage());
+                System.err.println("Use: RSAKeyPairGenerator <id> <key size>");
+            }
+
+        }
+}
+
