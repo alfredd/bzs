cf9393d6792112096ee2e869291a95b87754e087
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-04-20 12:59:39 -0700

    Adding Merkle Tree.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/MerkleBTreeManager.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/MerkleBTreeManager.java
@@ -1,15 +1,25 @@
 package edu.ucsc.edgelab.db.bzs.data;
 
+import com.google.protobuf.ByteString;
+import edu.ucsc.edgelab.db.bzs.Bzs;
+import merklebtree.MerkleBTree;
 import merklebtree.TreeNode;
 
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MerkleBTreeManager {
 
+    private Logger log = Logger.getLogger(MerkleBTreeManager.class.getName());
+
     private LinkedList<TreeNode> merkleTreeSnapshots;
+    private MerkleBTree merkleBTree;
 
     public MerkleBTreeManager() {
         merkleTreeSnapshots = new LinkedList<>();
+        merkleBTree = new MerkleBTree();
     }
 
     public void append(TreeNode root) {
@@ -19,4 +29,21 @@ public class MerkleBTreeManager {
     public TreeNode getLatest() {
         return merkleTreeSnapshots.peekLast();
     }
+
+
+    public Bzs.MerkleTree insert(String key, Bzs.DBData data) {
+        append(merkleBTree.root);
+        try {
+            byte[] rootHash = merkleBTree.put(key.getBytes(), data.toByteArray());
+
+            Bzs.MerkleTree tree = Bzs.MerkleTree.newBuilder()
+                    .setRoot(ByteString.copyFrom(rootHash))
+                    .build();
+            return tree;
+        } catch (IOException e) {
+            log.log(Level.WARNING,"Could not create Merkle tree node. "+e.getLocalizedMessage(),e);
+            return null;
+        }
+    }
+
 }
