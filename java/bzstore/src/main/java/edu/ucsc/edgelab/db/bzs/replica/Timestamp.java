4375811901bee71b522c83456554df0c165a8663
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-05-08 21:04:32 -0700

    Adding vector clock.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/Timestamp.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/Timestamp.java
@@ -1,9 +1,11 @@
 package edu.ucsc.edgelab.db.bzs.replica;
 
+import edu.ucsc.edgelab.db.bzs.Bzs;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Timestamp implements Comparable{
+public class Timestamp implements Comparable {
     private Map<Integer, Integer> timestamp = new LinkedHashMap<>();
 
     public void add(Integer clusterID, Integer epochNumber) {
@@ -19,4 +21,26 @@ public class Timestamp implements Comparable{
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
+
+    public Bzs.VectorClock getVectorTime() {
+        Bzs.VectorClock.Builder clockBuilder = Bzs.VectorClock.newBuilder();
+
+        for (Map.Entry<Integer, Integer> entry : timestamp.entrySet()) {
+            clockBuilder.addTimestamps(Bzs.Timestamp.newBuilder().setClusterID(entry.getKey()).setEpochNumber(entry.getValue()).build());
+        }
+        return clockBuilder.build();
+    }
+
+/*    public static void main(String[] args) {
+        Timestamp t = new Timestamp();
+        t.add(0, 4);
+        t.add(1, 3);
+        t.add(2, 5);
+        Bzs.VectorClock clock = t.getVectorTime();
+        for (int i = 0; i < clock.getTimestampsCount(); i++) {
+            System.out.println(String.format("%d -> %d", clock.getTimestamps(i).getClusterID(),
+                    clock.getTimestamps(i).getEpochNumber()));
+        }
+        System.out.println(clock.toString());
+    }*/
 }
