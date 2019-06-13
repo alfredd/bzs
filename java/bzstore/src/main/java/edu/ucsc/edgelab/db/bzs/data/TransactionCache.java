45938f13e3767336ea84e307b9ef1b3639c799c9
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-06-13 16:48:54 -0700

    Transaction processing implementation.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/TransactionCache.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/TransactionCache.java
@@ -2,32 +2,53 @@ package edu.ucsc.edgelab.db.bzs.data;
 
 import edu.ucsc.edgelab.db.bzs.Bzs;
 import edu.ucsc.edgelab.db.bzs.replica.TransactionID;
+import io.grpc.stub.StreamObserver;
 
 import java.util.TreeMap;
 
 public class TransactionCache {
     private static final TransactionCache CACHE = new TransactionCache();
-    private TreeMap<TransactionID, Bzs.Transaction> storage = new TreeMap<>();
+    private static class TxnTuple {
+        private Bzs.Transaction t;
+        private StreamObserver<Bzs.TransactionResponse> o;
+    }
+    private TreeMap<TransactionID, TxnTuple> storage = new TreeMap<>();
 
 
     private TransactionCache() {}
 
+    public static void add (TransactionID tid, Bzs.Transaction t, StreamObserver<Bzs.TransactionResponse> o) {
+        synchronized (CACHE) {
+            final TxnTuple tuple = new TxnTuple();
+            tuple.t=t;
+            tuple.o = o;
+            CACHE.storage.put(tid, tuple);
+        }
+    }
+
     public static void update (TransactionID tid, Bzs.Transaction t) {
         synchronized (CACHE) {
-            CACHE.storage.put(tid,t);
+            TxnTuple txnTuple = CACHE.storage.get(tid);
+            txnTuple.t = t;
         }
     }
 
-    public static Bzs.Transaction get(TransactionID tid) {
-//        synchronized (CACHE) {
-            return CACHE.storage.get(tid);
-//        }
+    public static Bzs.Transaction getTransaction(TransactionID tid) {
+        final TxnTuple txnTuple = CACHE.storage.get(tid);
+        return txnTuple==null? null: txnTuple.t;
     }
 
-    public static Bzs.Transaction remove(TransactionID tid) {
+    public static StreamObserver<Bzs.TransactionResponse> getObserver(TransactionID tid) {
+        final TxnTuple txnTuple = CACHE.storage.get(tid);
+        return txnTuple==null? null: txnTuple.o;
+    }
+
+    public static void removeHistory(TransactionID tid) {
         synchronized (CACHE) {
-            return CACHE.storage.remove(tid);
+            CACHE.storage.remove(tid);
         }
     }
+
+
 
 }
--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/txn/TransactionProcessor.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/txn/TransactionProcessor.java
@@ -6,12 +6,16 @@ import edu.ucsc.edgelab.db.bzs.replica.Serializer;
 import edu.ucsc.edgelab.db.bzs.replica.TransactionID;
 import io.grpc.stub.StreamObserver;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TransactionProcessor {
 
     private final Integer clusterID;
     private final Integer replicaID;
     private Serializer serializer;
     private EpochManager epochManager;
+    public static final Logger log = Logger.getLogger(TransactionProcessor.class.getName());
 
     public TransactionProcessor(Integer clusterID, Integer replicaID) {
         this.clusterID = clusterID;
@@ -20,11 +24,23 @@ public class TransactionProcessor {
         epochManager = new EpochManager();
     }
 
-    public void processTransaction(Bzs.Transaction request, StreamObserver<Bzs.TransactionResponse> responseObserver) {
-        if (!serializer.serialize(request)) {
+    public void processTransaction(final Bzs.Transaction request, final StreamObserver<Bzs.TransactionResponse> responseObserver) {
+        synchronized (this) {
 
+            if (!serializer.serialize(request)) {
+                log.info("Transaction cannot be serialized. Will abort. Request: " + request);
+                Bzs.TransactionResponse response =
+                        Bzs.TransactionResponse.newBuilder().setStatus(Bzs.TransactionStatus.ABORTED).build();
+                if (responseObserver != null) {
+                    responseObserver.onNext(response);
+                    responseObserver.onCompleted();
+                } else {
+                    log.log(Level.WARNING, "Transaction aborted: " + request.toString());
+                }
+                return;
+            }
+            TransactionID tid = epochManager.getTID();
+            TransactionCache.add(tid,request,responseObserver);
         }
-        TransactionID tid = epochManager.getTID();
-        TransactionCache.update(tid,request);
     }
 }
