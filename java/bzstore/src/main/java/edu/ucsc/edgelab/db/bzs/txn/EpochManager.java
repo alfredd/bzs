3f081b3d38476751126d8f545e4ad3e835280570
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-06-16 12:22:01 -0700

    Epoch manager.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/txn/EpochManager.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/txn/EpochManager.java
@@ -5,12 +5,25 @@ import edu.ucsc.edgelab.db.bzs.replica.TransactionID;
 public class EpochManager {
     private volatile Integer epochNumber = 0;
     private volatile Integer sequenceNumber = 0;
+    private volatile long epochStartTime = 0;
 
     public TransactionID getTID() {
         final Integer epochNumber = this.epochNumber;
         final Integer sequenceNumber = this.sequenceNumber;
         this.sequenceNumber+=1;
         return new TransactionID(epochNumber, sequenceNumber);
+    }
+
+    public void setEpochStartTime(final long epochStartTime) {
+        this.epochStartTime = epochStartTime;
+    }
+
+    public long getEpochStartTime() {
+        return epochStartTime;
+    }
+
+    public void updateEpoch() {
+        System.currentTimeMillis();
     }
 
     public void processEpoch() {
---
+++ java/bzstore/src/test/java/edu/ucsc/edgelab/db/bzs/txn/EpochManagerTest.java
@@ -0,0 +1,32 @@
+package edu.ucsc.edgelab.db.bzs.txn;
+
+import edu.ucsc.edgelab.db.bzs.replica.TransactionID;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EpochManagerTest {
+
+    @Test
+    public void testGetTID() {
+        EpochManager e = new EpochManager();
+        TransactionID tid1 = e.getTID();
+        TransactionID tid2 = e.getTID();
+        assertTrue(tid1.compareTo(tid2) < 0);
+        TransactionID tid3 = e.getTID();
+        assertEquals(-1, tid1.compareTo(tid2));
+        assertEquals(-2, tid1.compareTo(tid3));
+
+        assertEquals(2, tid3.compareTo(tid1));
+
+    }
+
+    @Test
+    public void testEpochTime() {
+        EpochManager e = new EpochManager();
+        final long epochStartTime = System.currentTimeMillis();
+        e.setEpochStartTime(epochStartTime);
+
+        assertEquals(epochStartTime, e.getEpochStartTime());
+    }
+}
