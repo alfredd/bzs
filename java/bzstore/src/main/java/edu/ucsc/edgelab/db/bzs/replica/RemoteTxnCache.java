e6a3d56acdb5f57a4269025cdec556445755ddc9
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-07-02 12:53:05 -0700

    Transaction processing implementation.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/RemoteTxnCache.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/RemoteTxnCache.java
@@ -7,22 +7,35 @@ import java.util.concurrent.ConcurrentSkipListSet;
 
 public class RemoteTxnCache {
 
-    private RemoteTxnCache() {}
+    private RemoteTxnCache() {
+    }
 
     private static Set<String> batchIDs = new ConcurrentSkipListSet<>();
 
     private static Map<String, Set<TransactionID>> preparedTIDsForBatch = new ConcurrentHashMap<>();
 
-    private static void addBatchIDToPrepared(String id) {
+    public static void addBatchIDToPrepared(String id) {
         batchIDs.add(id);
     }
 
+    public static boolean isPrepared(String id) {
+        return batchIDs.contains(id);
+    }
+
+    public static boolean isTIDInPreparedBatch(String id, TransactionID tid) {
+        if (! isPrepared(id))
+            return false;
+        return preparedTIDsForBatch.get(id).contains(tid);
+    }
+
     public static void addTIDsToPreparedBatch(String id, Set<TransactionID> preparedTIDs) {
-        if (!batchIDs.contains(id)) {
+        if (!isPrepared(id)) {
             batchIDs.add(id);
             preparedTIDsForBatch.put(id, preparedTIDs);
         } else {
             preparedTIDsForBatch.get(id).addAll(preparedTIDs);
         }
     }
+
+
 }
