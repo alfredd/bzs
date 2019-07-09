52b2af61de91c09baf6894e91686c9293255dec2
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-07-09 10:25:40 -0700

    SMR log reader

---
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/data/SmrLogReader.java
@@ -0,0 +1,20 @@
+package edu.ucsc.edgelab.db.bzs.data;
+
+import edu.ucsc.edgelab.db.bzs.Bzs;
+import org.rocksdb.RocksDBException;
+
+public class SmrLogReader {
+
+    public static void main(String[] args) throws RocksDBException {
+        if (args.length != 2)
+            System.exit(1);
+        Integer cid = Integer.decode(args[0]);
+        Integer rid = Integer.decode(args[1]);
+        BackendDb db = new BackendDb(cid, rid);
+        Integer epoch = db.getEpochNumber();
+        for (int i = 0; i <= epoch; i++) {
+            Bzs.SmrLogEntry smrlogEntry = db.getSmrBlock(epoch.toString());
+            System.out.println(String.format("Epoch: %d, SMR LOG[%d]: %s", epoch.intValue(), epoch.intValue(), smrlogEntry.toString()));
+        }
+    }
+}
