1e738e2b2b813ba736a73873463708805541ad2e
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-10-17 15:47:25 -0700

    Bug fix.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/BZStoreServer.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/replica/BZStoreServer.java
@@ -41,13 +41,14 @@ public class BZStoreServer {
         Integer clusterID = Integer.decode(args[0]);
         Integer replicaID = Integer.decode(args[1]);
         if (args.length==3) {
-            if ("y".equalsIgnoreCase(args[2])) {
+            if ("y".equalsIgnoreCase(args[2].trim())) {
                 runBenchmarks=true;
             } else {
                 printOptionsAndExit(args);
             }
         }
         ID.setRunBenchMarkTests(runBenchmarks);
+        logger.info("Benchmarks will be run? "+ ID.canRunBenchMarkTests());
         BZStoreProperties properties = new BZStoreProperties();
         try {
             Integer port = Integer.decode(
