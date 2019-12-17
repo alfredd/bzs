ff87f85b99b141737fa1b85fa0075ca279943acb
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-12-16 16:10:59 -0800

    Creating unit tests to test ROT execution.

--- java/bzstore/src/test/java/edu/ucsc/edgelab/db/bzs/clientlib/TestROTransaction.java
+++ java/bzstore/src/test/java/edu/ucsc/edgelab/db/bzs/clientlib/TestROTransaction.java
@@ -31,26 +31,28 @@ public class TestROTransaction {
                 Bzs.ROTransactionResponse.newBuilder()
                         .addReadResponses(
                                 Bzs.ReadResponse.newBuilder()
-                                        .setReadOperation(Bzs.Read.newBuilder().setKey("111").setClusterID(0).build())
-                                        .setValue("v1")
-                                        .putDepVector(0, 10)
-                                        .putDepVector(1, 7)
-                                        .putDepVector(2, 8)
-                                        .setLce(4)
+                                        .setReadOperation(Bzs.Read.newBuilder().setKey("22").setClusterID(0).build())
+                                        .setValue("v2")
+                                        .putDepVector(0, 7)
+                                        .putDepVector(1, 9)
+                                        .putDepVector(2, 4)
+                                        .setLce(5)
                                         .build())
                         .build());
         receivedResponses.put(2,
                 Bzs.ROTransactionResponse.newBuilder()
                         .addReadResponses(
                                 Bzs.ReadResponse.newBuilder()
-                                        .setReadOperation(Bzs.Read.newBuilder().setKey("111").setClusterID(0).build())
-                                        .setValue("v1")
-                                        .putDepVector(0, 10)
-                                        .putDepVector(1, 7)
-                                        .putDepVector(2, 8)
-                                        .setLce(4)
+                                        .setReadOperation(Bzs.Read.newBuilder().setKey("3").setClusterID(0).build())
+                                        .setValue("v3")
+                                        .putDepVector(0, 8)
+                                        .putDepVector(1, 6)
+                                        .putDepVector(2, 10)
+                                        .setLce(7)
                                         .build())
                         .build());
         List<Bzs.ReadResponse> secondROT = rot.generateSecondROTxns(receivedResponses);
+
+        System.out.println(secondROT);
     }
 }
