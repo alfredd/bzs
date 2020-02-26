09539d1b2caadd174a41ba84b07cab2a405cefe5
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2020-02-26 15:26:21 -0800

    Removing timer as epoch separator.

--- java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/txn/EpochManager.java
+++ java/bzstore/src/main/java/edu/ucsc/edgelab/db/bzs/txn/EpochManager.java
@@ -41,14 +41,14 @@ public class EpochManager {
         maxEpochBatchSize = Configuration.getEpochBatchCount();
         epochNumber = BZDatabaseController.getEpochCount() + 1;
         semaphore = new Semaphore(1);
-        TimerTask epochUpdater = new TimerTask() {
-            @Override
-            public void run() {
-                executeUpdateEpoch();
-            }
-        };
-        Timer t = new Timer();
-        t.scheduleAtFixedRate(epochUpdater, Configuration.getEpochTimeInMS(), Configuration.getEpochTimeInMS());
+//        TimerTask epochUpdater = new TimerTask() {
+//            @Override
+//            public void run() {
+//                executeUpdateEpoch();
+//            }
+//        };
+//        Timer t = new Timer();
+//        t.scheduleAtFixedRate(epochUpdater, Configuration.getEpochTimeInMS(), Configuration.getEpochTimeInMS());
         dTxnThreadPoolExecutor = new WedgeDBThreadPoolExecutor();
         DTxnCache.setEpochMetricsManager(batchMetricsManager);
     }
@@ -56,8 +56,8 @@ public class EpochManager {
     private void executeUpdateEpoch() {
         if (semaphore.tryAcquire()) {
             try {
-                if (sequenceNumber >= maxEpochBatchSize)
-                    updateEpoch();
+//                if (sequenceNumber >= maxEpochBatchSize)
+                updateEpoch();
             } finally {
                 semaphore.release();
             }
